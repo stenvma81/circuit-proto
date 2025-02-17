2e5ea29041a0837dbff99ebc5b2b50616f83bbd1
Matti Stenvall <matti.stenvall@gmail.com>
2025-02-17 18:32:34 +0200

    Add app template

---
+++ app/HoboCircuit/HoboCircuit/HoboCircuitApp.swift
@@ -0,0 +1,20 @@
+//
+//  HoboCircuitApp.swift
+//  HoboCircuit
+//
+//  Created by Matti Stenvall on 17.2.2025.
+//
+
+import SwiftUI
+
+@main
+struct HoboCircuitApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ app/HoboCircuit/HoboCircuitTests/HoboCircuitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoboCircuitTests.swift
+//  HoboCircuitTests
+//
+//  Created by Matti Stenvall on 17.2.2025.
+//
+
+import Testing
+@testable import HoboCircuit
+
+struct HoboCircuitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
